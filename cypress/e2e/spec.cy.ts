ee06a0fe356cefab930f673b1b7f7c73fea79fcc
Kevin Tan <relyzer@yahoo.com>
2022-06-20 20:53:17 +0800

    Updated readme

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,20 @@
 describe('Navigation', () => {
   it('user can navigate to series page and open a modal', () => {
     cy.visit('/')
+    cy.findByTestId('categoryNavLink-Series').should('exist')
     cy.findByTestId('categoryNavLink-Series').click()
+    cy.findByRole('article').contains('div').click()
+  })
+
+  it('user can navigate to movies page and open a modal', () => {
+    cy.visit('/')
+    cy.findByTestId('categoryNavLink-Movies').should('exist')
+    cy.findByTestId('categoryNavLink-Movies').click()
+    cy.findByRole('article').contains('div').click()
+  })
+
+  it('user can navigate back to home page', () => {
+    cy.visit('/series')
+    cy.findByText('DEMO Streaming').click()
   })
 })
-
-export {}
